c4f84433ddd55253e919f757b3cbca7fb203a747
aperezfals <aperezfals@gmail.com>
2020-06-26 14:37:05 +0300

    Add GetBookDetail Query

---
+++ BookStore.Application/Books/Queries/GetBookDetail/AuthorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStore.Application.Books.Queries.GetBookDetail
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+    }
+}
---
+++ BookStore.Application/Books/Queries/GetBookDetail/BookDetailVM.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Application.Books.Queries.GetBookDetail
+{
+    public class BookDetailVM
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public AuthorDTO Author { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
