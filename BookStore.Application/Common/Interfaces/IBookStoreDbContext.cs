b06d85a99c4e309998148e94a62959bacbcfa4f6
aperezfals <aperezfals@gmail.com>
2020-06-26 15:37:42 +0300

    Add application interfaces

---
+++ BookStore.Application/Common/Interfaces/IBookStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Application.Common.Interfaces
+{
+    public interface IBookStoreDbContext
+    {
+        DbSet<Book> Books { get; set; }
+
+        DbSet<Client> Clients { get; set; }
+
+        DbSet<Author> Authors { get; set; }
+
+        DbSet<Order> Orders { get; set; }
+    }
+}
