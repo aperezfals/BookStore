977d8f37fb4087ea5741929f464c592e4b632d69
aperezfals <aperezfals@gmail.com>
2020-06-26 15:38:01 +0300

    Add application dependency injection

---
+++ BookStore.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BookStore.Application.Common.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BookStore.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
